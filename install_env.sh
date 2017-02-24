07b4ddece5b8cb34bd3f22187287b35043674768
Daniel Kraemer Aliaga <daniel.aliaga@ge.com>
2017-02-24 15:43:18 -0300

    Add vimrc symbolic link

--- install_env.sh
+++ install_env.sh
@@ -18,22 +18,24 @@ SCRIPT_MSG=" [INSTALL ENV] "
 
 # Backup old configuration files
 echo $SCRIPT_MSG"Backup old config files"
-mkdir ~/.old
-mv ~/.bashrc    ~/.old/.bashrc.old
-mv ~/.gitconfig ~/.old/.gitconfig.old
-mv ~/.ssh       ~/.old/.ssh.old
-mv ~/.tmux.conf ~/.old/.tmux.conf.old
-mv ~/.vim       ~/.old/.vim.old
-mv ~/.vimrc     ~/.old/.vimrc.old
+#mkdir ~/.old
+#mv ~/.bashrc    ~/.old/.bashrc.old
+#mv ~/.gitconfig ~/.old/.gitconfig.old
+#mv ~/.ssh       ~/.old/.ssh.old
+#mv ~/.tmux.conf ~/.old/.tmux.conf.old
+#mv ~/.vim       ~/.old/.vim.old
+#mv ~/.vimrc     ~/.old/.vimrc.old
 
 # Create the Symbolic Links for new configuration files
 echo $SCRIPT_MSG"Create links for new config files"
 ln -s ~/.env_setup/configs/bash_aliases ~/.bash_aliases 
 ln -s ~/.env_setup/configs/gitconfig    ~/.gitconfig 
 ln -s ~/.env_setup/configs/tmux.conf    ~/.tmux.conf 
+ln -s ~/.env_setup/configs/ssh/config   ~/.ssh/config 
+ln -s ~/.vim/vimrc                      ~/.vimrc 
 
 # Protect SSH configuration
-#chmod 600 $DIR/ssh/*
+chmod 600 $DIR/ssh/*
 
 # Add custom scripts and binaries to PATH
 echo 'PATH=$PATH:$HOME/.env_setup/bin' >> .bashrc
